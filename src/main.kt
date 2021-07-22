5817a284a1d402f15dc6f1b893156f4d2805b6d2
HyeonseungSon <boywin8636@gmail.com>
2021-07-22 09:13:17 +0900

    Date / Time : 2021.07.22 09:13 Location : Man_Deum Description : Project for TypeCasting Test

--- src/main.kt
+++ src/main.kt
@@ -1,42 +1,48 @@
 fun main() {
+
     val a1 = "감사합니다."
 
-
-    // val chk1 = a1 is Int
+    // val chk1 = a1 is Int // 오류 발생
     val chk2 = a1 is String
 
     // println("chk1 : $chk1")
-    println("chk2 : $chk2")
-    println("a1 : $a1")
+    println("chk2 : $chk2") // chk2 : true
+    println("a1 : $a1") // a1 : 감사합니다.
 
-    val a2:SuperClass1 = SuperClass1()
-    val a3:SubClass2 = SubClass2()
+    val a2:SuperClass1 = SuperClass1() // 부모 Class
+    val a3:SubClass2 = SubClass2() // 자식 Class
 
     a2.superMethod1()
     a3.superMethod1()
     a3.subMethod2()
 
-    val chk3 = a2 is SubClass2
-    val chk4 = a3 is SuperClass1
+    val chk3 = a2 is SubClass2 // 부모 Class 를 자식 Class Type 으로 변경 => 불가능
+    val chk4 = a3 is SuperClass1 // 자식 Class 를 부모 Class Type 으로 변경 => 가능
 
-    println("chk3 : $chk3") /* 결과값 : false
+    println("chk3 : $chk3") /* chk3 : false
                 => 부모 Class 를 자식 Class 형으로 형변환 할 수 없다. */
+    println("chk4 : $chk4") /* chk4 : true
+                => 자식 Class 를 부모 Class 형으로 형변환할 수 있다. */
 
-    println("chk4 : $chk4") /* 결과값 : true
-                => 자식 Class 를 부모 Class 형으로 형변환할 수 있다.
-                             */
     a2.superMethod1()
     a3.superMethod1()
     a3.subMethod2()
+
+    println("------------------------------")
+
+    // 아래 if 문은 false 이기 때문에 실행되지 않습니다.
+    if (a2 is SubClass2) {
+        a2.superMethod1()
+        a2.subMethod2()
+    } else {
+        println("if (a2 is SubClass2) 은 실행되지 않습니다.")
+    }
+
+    println("------------------------------")
 
     if (a3 is SuperClass1) {
         a3.superMethod1()
         a3.subMethod2()
-    }
-
-    if (a2 is SubClass2) {
-        a2.superMethod1()
-        a2.subMethod2()
     }
 }
 
